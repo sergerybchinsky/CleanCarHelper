07b228f59c71143c22f5613bd83b328cbc7219cd
SergeRybchinsky <SergeRybchinsky@gmail.com>
2016-07-10 20:59:58 +0700

    Added core files

---
+++ CleanCarHelper/CleanCarHelper/App.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using MvvmCross.Platform.IoC;
+
+namespace CleanCarHelper.Core
+{
+	public class App : MvvmCross.Core.ViewModels.MvxApplication
+	{
+		public override void Initialize()
+		{
+			CreatableTypes()
+				.EndingWith("Service")
+				.AsInterfaces()
+				.RegisterAsLazySingleton();
+
+			RegisterAppStart<MainViewModel>();
+		}
+	}
+}
+
---
+++ CleanCarHelper/CleanCarHelper/ViewModels/MainViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using MvvmCross.Core.ViewModels;
+
+namespace CleanCarHelper.Core
+{
+	public class MainViewModel : MvxViewModel
+	{
+		private string _date = "Date";
+		public string Date
+		{
+			get { return _date; }
+			set { _date = value; RaisePropertyChanged(() => Date); }
+		}
+	}
+}
+
